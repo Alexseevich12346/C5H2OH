f7252f081a6e20dd03680648a945dce160e23b02
Alexseevich12346 <aalexseevich123@gmail.com>
2024-03-22 19:19:48 +0300

    add_main

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,31 +1,59 @@
 import '../App.css'; 
 import { Box } from '@chakra-ui/react';
 import NavBar from '../components/NavBar';
+import Vot from '../images/pc.png'
 import  Footer  from '../components/Footer'
-import { Flex } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 
 const HomePage = () =>{
     return(
         <>        
         <NavBar/>
         <div className="main">
-            <div className="main_hed">
-                <span>Первый курс <br />
-                      по компьютерной сборке
-                </span>
+            <div className="main_man">
+                <div className="main_hed">
+                    <span className='p'>Первый курс <br />
+                        по компьютерной сборке
+                    </span>
+                </div>
+                <div className="time">
+                    <div className="time_times">
+                        <span>18 <br />Дней</span>
+                    </div>
+                    <div className="time_times">
+                        <span>18 <br />Часов</span>
+                    </div>
+                    <div className="time_times">
+                        <span>18 <br />Минут</span>
+                    </div>
+                    <div className="time_times">
+                        <span>18 <br />Секунд</span>
+                    </div>
+                </div>
             </div>
-            <div className="time">
-                <div className="time_times">
-                    <span>18 <br />Дней</span>
+            <div className="kurs_po_obuch">
+                <div className="but_kurs">
+                    <button className='btnMain'>Заказать курс</button>
                 </div>
-                <div className="time_times">
-                    <span>18 <br />Часов</span>
+                <div className="ucheniki">
+                        <div className="uch_vsego">Учеников всего:       200</div>
+                        <div className="uch_ucpesh_okonch">Успешно закончили курс:   190</div>
                 </div>
-                <div className="time_times">
-                    <span>18 <br />Минут</span>
+                <div className="zarabotok"></div>
+            </div>
+            <div className="inf">
+                <div className="foto">
+                <Image                    
+                    textAlign={'center'}
+                    src={Vot}
+                    width={'300px'}
+                    alt='TheHost_IP-KVM'
+                >
+                </Image>
                 </div>
-                <div className="time_times">
-                    <span>18 <br />Секунд</span>
+                <div className="h1_inf">
+                    <h1>Чем мы занимаемся?</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus doloribus consectetur eos repellat, sapiente, exercitationem quia quod et officia hic similique, temporibus perspiciatis dolor. Iusto tenetur nesciunt sed facilis enim.</p>
                 </div>
             </div>
         </div>
