ee313103eab38b96ffa848b4af11ff5d9c88b455
Alexseevich12346 <aalexseevich123@gmail.com>
2024-03-24 17:19:07 +0100

    add_design

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -16,7 +16,7 @@ const HomePage = () =>{
                         по компьютерной сборке
                     </span>
                 </div>
-                <div className="time">
+                <div className="time" id='time'>
                     <div className="time_times">
                         <span>18 <br />Дней</span>
                     </div>
@@ -31,29 +31,44 @@ const HomePage = () =>{
                     </div>
                 </div>
             </div>
-            <div className="kurs_po_obuch">
-                <div className="but_kurs">
-                    <button className='btnMain'>Заказать курс</button>
+            <div className="main_two">
+                <div className="kurs_po_obuch">
+                    <div className="but_kurs">
+                        <button className='btnMain' src='#'>Заказать курс</button>
+                    </div>
+                    <div className="ucheniki">
+                            <div className="uch_vsego">Учеников всего:       200</div>
+                            <div className="uch_ucpesh_okonch">Успешно закончили курс:   190</div>
+                    </div>
+                    <div className="zarabotok">
+                        
+                    </div>
                 </div>
-                <div className="ucheniki">
-                        <div className="uch_vsego">Учеников всего:       200</div>
-                        <div className="uch_ucpesh_okonch">Успешно закончили курс:   190</div>
+                <div className="information">
+                    <div className="inf_op">
+                        <div className="foto">
+                        <Image                    
+                            textAlign={'center'}
+                            src={Vot}
+                            width={'300px'}
+                            alt='TheHost_IP-KVM'
+                            float={'left'}
+                        >
+                        </Image>
+                        </div>
+                        <div className="h1_inf">
+                            <h1>Чем мы занимаемся?</h1>
+                            <p>На курсах мы занимаемся обучением студентов основам и практическим навыкам сборки компьютеров. В рамках курсов мы познакомим вас с различными компонентами компьютера, их функциональностью и совместимостью, а также научим правильно выбирать и собирать комплектующие.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="zarabotok"></div>
-            </div>
-            <div className="inf">
-                <div className="foto">
-                <Image                    
-                    textAlign={'center'}
-                    src={Vot}
-                    width={'300px'}
-                    alt='TheHost_IP-KVM'
-                >
-                </Image>
-                </div>
-                <div className="h1_inf">
-                    <h1>Чем мы занимаемся?</h1>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus doloribus consectetur eos repellat, sapiente, exercitationem quia quod et officia hic similique, temporibus perspiciatis dolor. Iusto tenetur nesciunt sed facilis enim.</p>
+                <div className="start">
+                    <div className="start_inf">
+                        <div className="ellipse"></div>
+                        <h1>Быстрый старт</h1>
+                        <span className='nine_proc'>Больше 90% учеников прошли полный курс и смогли собрать свой первый компьютер</span>
+                    </div>
+                    .
                 </div>
             </div>
         </div>
