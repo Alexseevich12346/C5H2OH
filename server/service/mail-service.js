40cbdecb4fdf391ced7801248ad245fe72a4a452
Alexseevich12346 <aalexseevich123@gmail.com>
2024-04-26 19:28:44 +0300

    add_smtp_pass

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,4 +1,16 @@
+const nodemailer = require("nodemailer");
 class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "smtp.example.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "username",
+        pass: "password",
+      },
+    });
+  }
   async sendActivationMail(to, link) {}
 }
 module.exports = new MailService();
