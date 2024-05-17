183e01b1bf57699d95d514d7485de3b5de4aac24
Alexseevich12346 <aalexseevich123@gmail.com>
2024-05-17 14:04:29 +0200

    carousel_fix

---
+++ client/src/components/Carousel/CarouselEmbla.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+import { PrevButton, NextButton, usePrevNextButtons } from './EmblaArrowButtons';
+import styles from './EmblaCarousel.module.css';
+
+const EmblaCarousel = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <section className={styles.embla}>
+      <div className={styles.embla__viewport} ref={emblaRef}>
+        <div className={styles.embla__container}>
+          {slides.map((slide, index) => (
+            <div className={styles.embla__slide} key={index}>
+              {slide}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.embla__controls}>
+        <div className={styles.embla__buttons}>
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmblaCarousel;
--- client/src/pages/ShopPage.jsx
+++ client/src/pages/ShopPage.jsx
@@ -2,23 +2,38 @@ import '../App.css';
 import { Flex } from '@chakra-ui/react';
 import CardItem from '../components/cardItem';
 import NavBar from '../components/NavBar';
+import EmblaCarousel from '../components/Carousel/CarouselEmbla';
+import { Box } from '@chakra-ui/react';
+import ItemPage from '../images/pc.png'; 
+
+
+
 const ShopPage = () =>{
+  const IMAGES = [ItemPage, ItemPage, ItemPage, ItemPage, ItemPage]; // сюда кладешь фотки
+  const OPTIONS = {}
+  const SLIDES = IMAGES.map((src, index) => (
+    <img src={src} alt={`slide ${index}`} key={index} style={{ width: '500px', height: '500px' }} />
+  ));
   return (
     <Flex
       width={'100%'}
       flexWrap={'wrap'}
     >
-       <NavBar/>
-    <Flex
-        justify={'center'}
-        align={'center'}
-        flexWrap={'wrap'}
-        gap={'35px'}
-        mt={'50px'}
-    >
-    </Flex>
+      {/* <NavBar/> */}
+      {/* <Flex
+          justify={'center'}
+          align={'center'}
+          flexWrap={'wrap'}
+          gap={'35px'}
+          mt={'50px'}
+      > */}
+        <Flex mt='50px'>
+          <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+        </Flex>
+      {/* </Flex> */}
     </Flex>
    
   );
 }
 export default ShopPage;
+
